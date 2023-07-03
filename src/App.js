10de15124510cda343f949a9adda5729e4d3bd6b
pittu777 <you@example.com>
2023-07-03 13:55:52 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import "./App.css"
+import Search from "./components/search/Search";
+import Forecast from "./components/forecast/forecast";
+import "./index.css"
+import {WEATHER_API_URL, WEATHER_API_KEY} from "./Api"
+import CurrentWeather from "./components/current-weather/current-weather";
+function App(){
+
+  const [currentweather, setCurrentweather]=React.useState(null);
+  const [forcast, setForcast]=React.useState(null);
+
+  const handleOnSearchChange=(searchdata)=>{
+    const [lat, lon]=searchdata.value.split(" ");
+
+    const currentweatherfetch=fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+    const forcastfetch=fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+    
+    Promise.all([currentweatherfetch, forcastfetch])
+      .then(async(response)=>{
+        const weatherResponse = await response[0].json();
+        const forcastResponse = await response[1].json();
+
+        setCurrentweather({city: searchdata.label, ...weatherResponse});
+        setForcast({city: searchdata.label, ...forcastResponse});
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
+
+    
+  }
+  console.log(currentweather);
+  console.log(forcast)
+  return(
+    <div >
+      <Search onSearchChange={handleOnSearchChange}/>
+
+      { currentweather && <CurrentWeather data={currentweather}/>}
+      
+      {forcast && <Forecast  data={forcast}/>}
     </div>
-  );
+  )
 }
-
 export default App;
