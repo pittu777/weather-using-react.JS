874fcba92a9123063f010795ad18d9c544a87aea
pittu777 <you@example.com>
2023-08-29 23:17:14 +0530

    update

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,20 +6,21 @@ import wind from "./images/wind.png";
 import time from "./images/logo-time.png";
 import "./../forecast/forecast.css";
 import { FaFacebook, FaInstagram, FaTwitter, FaBug } from "react-icons/fa";
+import  Logo  from "./images/logo1.png";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
         <div className="footer-section contact">
-        <div className="report-heading">
+          <div className="report-heading">
             <h3>Report</h3>
             <FaBug className="bug-icon" />
           </div>
           <ContactForm />
           <p>Email: pittuprasanth14@gmail.com</p>
         </div>
-        
+
         <div className="footer-section social-media">
           <h3>Follow Us</h3>
           <div className="social-icons">
@@ -68,6 +69,21 @@ const Footer = () => {
               </a>
             </p>
             <p className="body-3">
+              <a
+                href="https://rapidapi.com/hub"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <img
+                  src={Logo}
+                  alt=""
+                  width={150}
+                  height={30}
+                  loading="lazy"
+                />
+              </a>
+            </p>
+            <p className="body-3">
               <a href="https://www.windy.com/" rel="noreferrer" target="_blank">
                 <img src={wind} alt="" width={150} height={30} loading="lazy" />
               </a>
@@ -89,38 +105,29 @@ const Footer = () => {
         {/* <p>&copy; 2023 PITTU PRASANTH or PRAKASH,PRAVEEN,SIVA,NISHANTH. All rights reserved.</p>
         <p>Website designed and developed by my team and me</p> */}
         <div className="gap">
-        <div className="footer-section weather-gif">
-          {/* Display the weather GIF */}
-          <img
-            className="weather-gif-image"
-            src={openLogo}
-            alt="Weather GIF"
-          />
-          
-        </div>
-        <div className="footer-section weather-gif">
-          {/* Display the weather GIF */}
-          <img
-            className="weather-gif-image"
-            src={wind}
-            alt="Weather GIF"
-          />
-          
-        </div>
-        <div className="footer-section weather-gif">
-          {/* Display the weather GIF */}
-          <img
-            className="weather-gif-image"
-            src={time}
-            alt="Weather GIF"
-          />
-          
-        </div>
-        </div>
-        <p className="p">&copy; 2023 PITTU PRASANTH or PRAKASH,PRAVEEN,SIVA,NISHANTH. All rights reserved.</p>
-        <p className="p">Website designed and developed by my team and me</p>
-        
+          <div className="footer-section weather-gif">
+            {/* Display the weather GIF */}
+            <img
+              className="weather-gif-image"
+              src={openLogo}
+              alt="Weather GIF"
+            />
+          </div>
+          <div className="footer-section weather-gif">
+            {/* Display the weather GIF */}
+            <img className="weather-gif-image" src={wind} alt="Weather GIF" />
+          </div>
 
+          <div className="footer-section weather-gif">
+            {/* Display the weather GIF */}
+            <img className="weather-gif-image" src={Logo} alt="Weather GIF" />
+          </div>
+        </div>
+        <p className="p">
+          &copy; 2023 PITTU PRASANTH or PRAKASH,PRAVEEN,SIVA,NISHANTH. All
+          rights reserved.
+        </p>
+        <p className="p">Website designed and developed by my team and me</p>
       </div>
     </footer>
   );
